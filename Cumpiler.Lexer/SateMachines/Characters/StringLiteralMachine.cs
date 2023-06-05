7c693e38c9f27b59c2c852eebe19005d3fc06805
Alex <alex@hagl.de>
2023-06-06 01:00:29 +0200

    Added String literal machine

---
+++ Cumpiler.Lexer/SateMachines/Characters/StringLiteralMachine.cs
@@ -0,0 +1,30 @@
+﻿using Cumpiler.Lexer.Common.Abstract;
+using Cumpiler.Lexer.Common.Tokens;
+
+namespace Cumpiler.Lexer.SateMachines.Characters {
+    internal class StringLiteralMachine : StateMachine {
+
+        public override TokenType GetTokenType() {
+            return TokenType.STRING;            
+        }
+
+        protected override void InitStateTable() {
+            _start = new State("start", false);
+            var innerState = AddState(new State("inner", false));
+            var escapedState = AddState(new State("escaped", false));
+            var endState = AddState(new State("end", true));
+
+            _start.AddTransition(innerState, '\"');
+
+            innerState
+                .AddTransitionRange(innerState, ' ', '!')
+                .AddTransitionRange(innerState, '#', '[')
+                .AddTransitionRange(innerState, ']', '~')
+                .AddTransition(escapedState, '\\')
+                .AddTransition(endState, '\"');
+
+            escapedState
+                .AddTransition(innerState, 't', 'b', 'n', 'r', 'f', '\'', '"', '\\');
+        }
+    }
+}
---
+++ Cumpiler.UnitTests/Lexer/StateMachines/StringLiteralMachineTest.cs
@@ -0,0 +1,52 @@
+﻿using Cumpiler.Lexer.SateMachines.Characters;
+
+namespace Cumpiler.UnitTests.Lexer.StateMachines {
+    public class StringLiteralMachineTest {
+
+        [Fact]
+        public void StringLiteral_ShouldAccept() {
+            var machine = new StringLiteralMachine();
+
+            machine.Process("\"'Hello' World\"");
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Theory]
+        [InlineData("\"\"")]
+        [InlineData("\" \"")]
+        public void StringLiteral_ShouldAccept_Empty(string input) {
+            var machine = new StringLiteralMachine();
+
+            machine.Process(input);
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void StringLiteral_ShouldNotAccept_WrongEscapeChars() {
+            var machine = new StringLiteralMachine();
+
+            machine.Process("\"\\lHello World\"");
+
+            Assert.False(machine.IsInFinalState());
+        }
+
+        [Theory]
+        [InlineData("\"\\t\"")]
+        [InlineData("\"\\b\"")]
+        [InlineData("\"\\n\"")]
+        [InlineData("\"\\r\"")]
+        [InlineData("\"\\f\"")]
+        [InlineData("\"\\\'\"")]
+        [InlineData("\"\\\"\"")]
+        [InlineData("\"\\\\\"")]
+        public void StringLiteral_ShouldAccept_EscapeCharacters(string input) {
+            var machine = new StringLiteralMachine();
+
+            machine.Process(input);
+
+            Assert.True(machine.IsInFinalState());
+        }
+    }
+}
