099e74d3724fa7dcfddf5b4e67fdade00f5bc787
Alex <alex@hagl.de>
2023-06-06 01:40:20 +0200

    Added Integer Machine

---
+++ Cumpiler.Lexer/SateMachines/Numbers/IntegerMachine.cs
@@ -0,0 +1,88 @@
+﻿using Cumpiler.Lexer.Common.Abstract;
+using Cumpiler.Lexer.Common.Tokens;
+
+namespace Cumpiler.Lexer.SateMachines.Numbers {
+    internal class IntegerMachine : StateMachine {
+
+        public override TokenType GetTokenType() {
+            return TokenType.INTEGER;
+        }
+
+        protected override void InitStateTable() {
+            /* Accepts:
+             * 0
+             * 00012345
+             * 12343_100_000
+             * 
+             * 0xABCDEF001023
+             * 0xABC_DEF
+             * 0b01001001010
+             * 0b010_1011
+             * 
+             * Does not accept:
+             * 0x
+             * 0b
+             * 1234_
+             * 0b1234
+             * 0x12354Y
+             * 1235Y
+             */
+
+            _start = AddState(new State("start", false));
+            var decimalSystemNumberState = AddState(new State("decimalNumber", true));
+            var decimalUnderscoreState = AddState(new State("decimalUnderscore", false));
+            var firstZeroState = AddState(new State("firstZero", true));
+
+            var binaryNumberFirstState = AddState(new State("binaryNumberFirst", false));
+            var binaryNumberState = AddState(new State("binaryNumber", true));
+            var binaryUnderscoreState = AddState(new State("binaryUnderscore", false));
+
+            var hexNumberFirstState = AddState(new State("hexNumberFirst", false));
+            var hexNumberState = AddState(new State("hexNumber", true));
+            var hexNumberUnderscoreState = AddState(new State("hexNumberUnderscore", false));
+
+            _start
+                .AddTransitionRange(decimalSystemNumberState, '1', '9')
+                .AddTransition(firstZeroState, '0');
+
+            decimalSystemNumberState
+                .AddTransitionRange(decimalSystemNumberState, '0', '9')
+                .AddTransition(decimalUnderscoreState, '_');
+
+            decimalUnderscoreState
+                .AddTransitionRange(decimalSystemNumberState, '0', '9');
+
+            firstZeroState
+                .AddTransitionRange(decimalSystemNumberState, '0', '9')
+                .AddTransition(binaryNumberFirstState, 'b', 'B')
+                .AddTransition(hexNumberFirstState, 'x', 'X');
+
+            binaryNumberFirstState
+                .AddTransition(binaryNumberState, '0', '1');
+
+            binaryNumberState
+                .AddTransition(binaryNumberState, '0', '1')
+                .AddTransition(binaryUnderscoreState, '_');
+
+            binaryUnderscoreState
+                .AddTransition(binaryNumberState, '0', '1');
+
+            hexNumberFirstState
+                .AddTransitionRange(hexNumberState, '0', '9')
+                .AddTransitionRange(hexNumberState, 'A', 'F')
+                .AddTransitionRange(hexNumberState, 'a', 'f');
+
+            hexNumberState
+                .AddTransitionRange(hexNumberState, '0', '9')
+                .AddTransitionRange(hexNumberState, 'A', 'F')
+                .AddTransitionRange(hexNumberState, 'a', 'f')
+                .AddTransition(hexNumberUnderscoreState, '_');
+
+            hexNumberUnderscoreState
+                .AddTransitionRange(hexNumberState, '0', '9')
+                .AddTransitionRange(hexNumberState, 'A', 'F')
+                .AddTransitionRange(hexNumberState, 'a', 'f');
+
+        }
+    }
+}
---
+++ Cumpiler.UnitTests/Lexer/StateMachines/IntegerMachineTest.cs
@@ -0,0 +1,148 @@
+﻿using Cumpiler.Lexer.SateMachines.Numbers;
+
+namespace Cumpiler.UnitTests.Lexer.StateMachines {
+    public class IntegerMachineTest {
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("1")]
+        [InlineData("01")]
+        [InlineData("0923")]
+        [InlineData("90")]
+        [InlineData("1234567890")]
+        public void Machine_ShouldAccept_DecimalInteger(string input) {
+            var machine = new IntegerMachine();
+
+            machine.Process(input);
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldAccept_DecimalNumber_WithUnderscores() {
+            var machine = new IntegerMachine();
+
+            machine.Process("123_234_4512344_2");
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Hello World")]
+        public void Machine_ShouldNotAccept_NonNumber(string input) {
+            var machine = new IntegerMachine();
+
+            machine.Process(input);
+
+            Assert.False(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_Decimal_WithUnderscoreAtEnd() {
+            var machine = new IntegerMachine();
+
+            machine.Process("1234_");
+
+            Assert.False(machine.IsInFinalState());
+        }
+
+        [Theory]
+        [InlineData("0b0101")]
+        [InlineData("0b0")]
+        [InlineData("0b11111111111")]
+        [InlineData("0b0000000000")]
+        public void Machine_ShouldAccept_BinaryNumber(string input) {
+            var machine = new IntegerMachine();
+
+            machine.Process(input);
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldAccept_BinaryNumber_WithUnderscore() {
+            var machine = new IntegerMachine();
+
+            machine.Process("0b10_10010_1000");
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_BinaryNumber_WithUnderscoreAtEnd() {
+            var machine = new IntegerMachine();
+
+            machine.Process("0b10_10010_1000_");
+
+            Assert.False(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_NonBinaryNumber() {
+            var machine = new IntegerMachine();
+
+            machine.Process("0b120");
+
+            Assert.False(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_NoBinaryNumber() {
+            var machine = new IntegerMachine();
+
+            machine.Process("0b");
+
+            Assert.False(machine.IsInFinalState());
+        }
+
+
+        [Theory]
+        [InlineData("0x1230")]
+        [InlineData("0x000")]
+        [InlineData("0x0123456789ABCDEF0")]
+        public void Machine_ShouldAccept_HexNumber(string input) {
+            var machine = new IntegerMachine();
+
+            machine.Process(input);
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_NonHexNumber() {
+            var machine = new IntegerMachine();
+
+            machine.Process("0x01Z");
+
+            Assert.False(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_NoHexNumber() {
+            var machine = new IntegerMachine();
+
+            machine.Process("0x");
+
+            Assert.False(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldAccept_HexNumber_WithUnderscore() {
+            var machine = new IntegerMachine();
+
+            machine.Process("0x10_10010_1000");
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_HexNumber_WithUnderscoreAtEnd() {
+            var machine = new IntegerMachine();
+
+            machine.Process("0x10_10010_1000_");
+
+            Assert.False(machine.IsInFinalState());
+        }
+    }
+}
