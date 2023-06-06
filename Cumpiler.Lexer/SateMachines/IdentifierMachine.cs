c751b70327f1982fbfac88e49a3548c000bc96ea
Alex <alex@hagl.de>
2023-06-06 02:24:37 +0200

    Added Identifier machine

---
+++ Cumpiler.Lexer/SateMachines/IdentifierMachine.cs
@@ -0,0 +1,28 @@
+﻿using Cumpiler.Lexer.Common.Abstract;
+using Cumpiler.Lexer.Common.Tokens;
+
+namespace Cumpiler.Lexer.SateMachines {
+    internal class IdentifierMachine : StateMachine {
+
+        public override TokenType GetTokenType() {
+            return TokenType.IDENTIFIER;
+        }
+
+        protected override void InitStateTable() {
+
+            _start = AddState(new State());
+            var endState = AddState(new State(true));
+
+            _start
+                .AddTransition(endState, '_')
+                .AddTransitionRange(endState, 'a', 'z')
+                .AddTransitionRange(endState, 'A', 'Z');
+
+            endState
+                .AddTransition(endState, '_')
+                .AddTransitionRange(endState, 'a', 'z')
+                .AddTransitionRange(endState, 'A', 'Z')
+                .AddTransitionRange(endState, '0', '9');
+        }
+    }
+}
---
+++ Cumpiler.UnitTests/Lexer/StateMachines/IdentifierMachineTest.cs
@@ -0,0 +1,36 @@
+﻿using Cumpiler.Lexer.SateMachines;
+
+namespace Cumpiler.UnitTests.Lexer.StateMachines {
+    public class IdentifierMachineTest {
+
+        [Theory]
+        [InlineData("_")]
+        [InlineData("asd_d02_2")]
+        [InlineData("AdwdASdwXACwcw123")]
+        public void Machine_ShouldAccept_IdentifierName(string input) {
+            var machine = new IdentifierMachine();
+
+            machine.Process(input);
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_NumberAsFirst() {
+            var machine = new IdentifierMachine();
+
+            machine.Process("0ad_wd");
+
+            Assert.False(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_SpecialChar() {
+            var machine = new IdentifierMachine();
+
+            machine.Process("$ad_wd");
+
+            Assert.False(machine.IsInFinalState());
+        }
+    }
+}
