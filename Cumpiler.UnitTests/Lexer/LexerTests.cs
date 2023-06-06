8d1fd8b09576642339320c11dc20281a4e41f9d2
Alex <alex@hagl.de>
2023-06-06 03:10:49 +0200

    Lexer test

--- Cumpiler.UnitTests/Lexer/LexerTests.cs
+++ Cumpiler.UnitTests/Lexer/LexerTests.cs
@@ -1,8 +1,22 @@
 ﻿
+using Cumpiler.Lexer;
+using Cumpiler.Lexer.Common.Tokens;
+
 namespace Cumpiler.UnitTests.Lexer {
     public class LexerTests {
 
-        
+        [Fact]
+        public void Lexer_MatchesKeywordsBeforeIdentifier() {
+            var lexer = LexerFactory.CreateLexer("IF FUN heLoWorld wasGeht 1");
+
+            var tokens = lexer.AdvanceTillEOF();
+
+            var expected = new List<TokenType> {
+                TokenType.IF, TokenType.FUN, TokenType.IDENTIFIER, TokenType.IDENTIFIER, TokenType.INTEGER, TokenType.EOF
+            };
+
+            Assert.Equal(tokens.Select(e => e.Type).ToList(), expected);
+        }
 
     }
 }
