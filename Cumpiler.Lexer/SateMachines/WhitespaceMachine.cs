2a1a7fd0e69bb662e72952c7aa2a94fb311a3310
Alex <alex@hagl.de>
2023-06-06 02:33:49 +0200

    Added whitespace machine

---
+++ Cumpiler.Lexer/SateMachines/WhitespaceMachine.cs
@@ -0,0 +1,22 @@
+﻿using Cumpiler.Lexer.Common.Abstract;
+using Cumpiler.Lexer.Common.Tokens;
+
+namespace Cumpiler.Lexer.SateMachines {
+    internal class WhitespaceMachine : StateMachine {
+
+        public override TokenType GetTokenType() {
+            return TokenType.WHITESPACE;
+        }
+
+        protected override void InitStateTable() {
+            _start = AddState(new State());
+            var whiteSpace = AddState(new State(true));
+
+            _start
+                .AddTransition(whiteSpace, (char)9, (char)10, (char)11, (char)12, (char)13, (char)32);
+
+            whiteSpace
+                .AddTransition(whiteSpace, (char)9, (char)10, (char)11, (char)12, (char)13, (char)32);
+        }
+    }
+}
---
+++ Cumpiler.UnitTests/Lexer/StateMachines/WhitespaceMachineTest.cs
@@ -0,0 +1,28 @@
+﻿
+using Cumpiler.Lexer.SateMachines;
+
+namespace Cumpiler.UnitTests.Lexer.StateMachines {
+    public class WhitespaceMachineTest {
+
+        [Theory]
+        [InlineData(" ")]
+        [InlineData("       ")]
+        [InlineData(" \n\r\t")]
+        public void Machine_ShouldAccept_WhitespaceVariants(string input) {
+            var machine = new WhitespaceMachine();
+
+            machine.Process(input);
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        [Fact]
+        public void Machine_ShouldNotAccept_Text() {
+            var machine = new WhitespaceMachine();
+
+            machine.Process(" Hello World ");
+
+            Assert.False(machine.IsInFinalState());
+        }
+    }
+}
