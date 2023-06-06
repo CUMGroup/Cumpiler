cc009775355d0fb991b241a07e98fcffaccee087
Alex <alex@hagl.de>
2023-06-06 02:46:14 +0200

    Added LineCommentMachine

---
+++ Cumpiler.Lexer/SateMachines/Comments/LineCommentMachine.cs
@@ -0,0 +1,28 @@
+﻿using Cumpiler.Lexer.Common.Abstract;
+using Cumpiler.Lexer.Common.Tokens;
+
+namespace Cumpiler.Lexer.SateMachines.Comments {
+    internal class LineCommentMachine : StateMachine {
+        public override TokenType GetTokenType() {
+            return TokenType.LINECOMMENT;
+        }
+
+        protected override void InitStateTable() {
+            _start = AddState(new State());
+            var oneSlash = AddState(new State());
+            var innerComment = AddState(new State());
+            var end = AddState(new State(true));
+
+            _start
+                .AddTransition(oneSlash, '/');
+
+            oneSlash
+                .AddTransition(innerComment, '/');
+
+            innerComment
+                .AddTransitionRange(innerComment, ' ', '~')
+                .AddTransition(innerComment, 'ä', 'ö', 'ü', 'ß')
+                .AddTransition(end, '\n', '\0');
+        }
+    }
+}
---
+++ Cumpiler.UnitTests/Lexer/StateMachines/LineCommentMachineTest.cs
@@ -0,0 +1,27 @@
+﻿using Cumpiler.Lexer.SateMachines.Comments;
+
+namespace Cumpiler.UnitTests.Lexer.StateMachines {
+    public class LineCommentMachineTest {
+
+        [Theory]
+        [InlineData("//\n")]
+        [InlineData("//asdwd$%wölkfnsfö\n")]
+        [InlineData("//   \n")]
+        [InlineData("// asdw dw\n")]
+        public void Machine_ShouldAccept_Comment(string input) {
+            var machine = new LineCommentMachine();
+
+            machine.Process(input);
+
+            Assert.True(machine.IsInFinalState());
+        }
+
+        public void Machine_ShouldNotAccept_OneSlash() {
+            var machine = new LineCommentMachine();
+
+            machine.Process("/ dawdw\n");
+
+            Assert.False(machine.IsInFinalState());
+        }
+    }
+}
